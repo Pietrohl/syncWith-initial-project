40e2ea85e9e76a0aa6a78a7ae043929557dc3ea8
Pietro Henrique <pietro_henrique4@hotmail.com>
2022-03-12 16:50:07 -0300

    refactor app class

---
+++ src/middlewares/logger.ts
@@ -0,0 +1,59 @@
+import morgan, { StreamOptions } from 'morgan'
+import winston from 'winston'
+/*
+ * Winston Middleware
+ */
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'white'
+}
+winston.addColors(colors)
+
+const logger = winston.createLogger({
+  level: (() => {
+    const env = process.env.NODE_ENV || 'development'
+    const isDevelopment = env === 'development'
+    return isDevelopment ? 'debug' : 'warn'
+  })(),
+  levels: {
+    error: 0,
+    warn: 1,
+    info: 2,
+    http: 3,
+    debug: 4
+  },
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+    winston.format.colorize({ all: true }),
+    winston.format.printf(
+      info => `${info.timestamp} ${info.level}: ${info.message}`
+    )
+  ),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'error'
+    }),
+    new winston.transports.File({ filename: 'logs/all.log' })
+  ]
+})
+
+/*
+ * Morgan Middleware
+ */
+
+const stream: StreamOptions = {
+  write: message => logger.http(message)
+}
+
+const m = () =>
+  morgan('tiny', {
+    stream: stream
+  })
+
+export default m
---
+++ src/types/middleware/index.d.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express'
+
+export type ExpressRouter = {
+  routeName: string
+  router: Router
+}
